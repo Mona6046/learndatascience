d122200c9cf1f0c8ac1f1f7aa381c1523689b6c6
Mona6046 <34856313+Mona6046@users.noreply.github.com>
2018-10-24 14:24:15 +0530

    VerizonTechgig

--- VerizonTechgig/PredictUserAction.py
+++ VerizonTechgig/PredictUserAction.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -5,29 +6,39 @@ import random
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import roc_curve,auc,roc_auc_score
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+
 import os
 cwd = os.getcwd()
 print(cwd)
+
 train=pd.read_csv(cwd+'\\training_data.csv')
 test=pd.read_csv(cwd+'\\test_data.csv')
+
 train['Type']='Train'
 test['Type']='Test'
 fullData = pd.concat([train,test],axis=0,sort=False)
 fullData2= fullData
-fullData=fullData[fullData['id']!= 'session************']
+fullData=fullData[(fullData['id']!= 'session************')]
+
+fullData.columns
+fullData.dtypes
+
+
 fullData['timestamp']=pd.to_datetime(fullData['timestamp'], format='%Y-%m-%dT%H:%M:%S.%f')+ pd.Timedelta('-04:00:00')
 fullData['hour'] = fullData['timestamp'].dt.hour
 fullData.head(10)
-#0 for lower, 1 for middle, 2 for upper
-# creating a dict   
-Funnel_level = {'lower': 0,'middle': 2,'upper': 2} 
+Funnel_level = {'lower': 2,'middle': 1,'upper': 0} 
 fullData['funnel_level'] = [Funnel_level[item] for item in fullData['funnel_level'] ] 
 fullData.info()
 fullData.isnull().any()
 fullData.describe()
 fullData.describe(include=['O'])
 fullData[['link', 'order_placed']].groupby(['link'], as_index=False).mean().sort_values(by='link', ascending=True)
+
 ID_col = ['id']
 target_col = ['order_placed'] 
 cat_cols = ['ad','link','hour','grp']
@@ -52,14 +63,17 @@ x_validate = Validate[list(features)].values
 y_validate = Validate["order_placed"].values
 x_test=test[list(features)].values
 x_train
+x_test
+label=['checkout','order_placed']
 random.seed(100)
-rf = GradientBoostingClassifier(n_estimators=1000)
-rf.fit(x_train, y_train)
-features
-status = rf.predict_proba(x_validate)
-fpr, tpr, _ = roc_curve(y_validate, status[:,1])
-roc_auc = auc(fpr, tpr)
-print (roc_auc)
-final_status = rf.predict_proba(x_test)
-test["order_placed"]=final_status[:,1]
+score=[]
+for code in label:
+    rf = GradientBoostingClassifier(n_estimators=800)
+    rf.fit(x_train,Train[code])
+    #prediction = rf.predict(x_test)
+    #score.append(roc_auc_score(Test[code],prediction))
+    final_status = rf.predict_proba(x_test)
+    test[code]=final_status[:,1]
+    
+test.to_csv(cwd+'\\gbc_model_output2.csv',columns=['id','checkout','order_placed'])
 test.to_csv(cwd+'\\gbc_model_output.csv',columns=['id','order_placed'])
